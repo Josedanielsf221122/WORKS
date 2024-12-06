66e9cd960c7926987b2334df88853b1ed6b310cf
Josedanielsf221122 <139817212+Josedanielsf221122@users.noreply.github.com>
2024-12-05 20:25:57 -0400

    Delete Clientes.py

--- Clientes.py
+++
@@ -1,9 +0,0 @@
-
-class Cliente:
-    def __init__(self, nombre,cantidad,comida,total, precio):
-        self.nombre = nombre
-       # self.precio = precio
-        self.comida = comida
-        self.cantidad = cantidad
-        self.total = total
-        self.atendido = "En espera"
