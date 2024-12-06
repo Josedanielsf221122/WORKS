024d1a35abb3b9b9b9c86abd37c3aa8b05189c40
Josedanielsf221122 <139817212+Josedanielsf221122@users.noreply.github.com>
2024-12-05 20:19:12 -0400

    Add files via upload

---
+++ Clientes.py
@@ -0,0 +1,9 @@
+
+class Cliente:
+    def __init__(self, nombre,cantidad,comida,total, precio):
+        self.nombre = nombre
+       # self.precio = precio
+        self.comida = comida
+        self.cantidad = cantidad
+        self.total = total
+        self.atendido = "En espera"
